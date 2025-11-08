b1d5b9060c1248e2c80e9923ed7110db03d66d18
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-08 16:13:00 +0530

    Room creation logic added

---
+++ src/Pages/Room.tsx
@@ -0,0 +1,21 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { SocketContext } from "../Context/SocketContext";
+
+const Room: React.FC = () => {
+
+    const { id } = useParams();
+    const { socket } = useContext(SocketContext);
+
+    useEffect(() => {
+        socket.emit("joined-room", { roomId: id });
+    },[]);
+
+    return (
+        <div>
+            Room Page: {id}
+        </div>
+    )
+}
+
+export default Room;
