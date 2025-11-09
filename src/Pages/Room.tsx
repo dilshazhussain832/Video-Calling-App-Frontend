0e9e23e6eeb298b1041d452330a3aec4c67342b3
Dilshaz_hussain832 <dilshazhussain123@gmail.com>
2025-11-09 15:01:34 +0530

    Room creation logic with user joining the room and there record of joining being stored added

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -5,11 +5,11 @@ import { SocketContext } from "../Context/SocketContext";
 const Room: React.FC = () => {
 
     const { id } = useParams();
-    const { socket } = useContext(SocketContext);
+    const { socket, user } = useContext(SocketContext);
 
     useEffect(() => {
-        socket.emit("joined-room", { roomId: id });
-    },[]);
+        if(user) socket.emit("joined-room", { roomId: id, peerId: user._id });
+    },[id, user, socket]);
 
     return (
         <div>
